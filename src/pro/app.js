f80745679c4b6a97779fca58d72419c5e17c6506
walker <weikuo0506@163.com>
2017-04-13 18:07:28 +0800

    add inverse http web proxy

--- src/pro/app.js
+++ src/pro/app.js
@@ -3,9 +3,25 @@
  */
 var express = require('express');
 var PORT = 1234;
+
+var zookeeper = require('node-zookeeper-client');
+var CONNECTION_STR = '127.0.0.1:2181';
+var REGISTRY_ROOT = '/microservices';
+
+//get zk client and connect to zk
+var zk = zookeeper.createClient(CONNECTION_STR);
+zk.connect();
+
+//proxy server
+var httpProxy = require('http-proxy');
+var proxy = httpProxy.createProxyServer();
+proxy.on('error',function(err,req,res){
+    res.end();
+})
+
 //start web server
 var app = express();
-app.use(express.static('public'));
+app.use(express.static('public'));   //be careful with the static dir;
 app.all('*',function(req,res){
     //ignore favicon
     if(req.path == '/favicon.ico'){
@@ -21,7 +37,50 @@ app.all('*',function(req,res){
         return;
     }
     //do proxy dynamicly
+    //1. get serivce path in zk
+    var servicePath = REGISTRY_ROOT + '/' + serviceName;
+    console.log('servicePath: %s', servicePath);
+    //2. get service addr path in zk
+    zk.getChildren(servicePath, function (err, addrNodes) {
+        if(err) {
+            console.log("get service addr error: "+err.stack);
+            res.end();
+            return;
+        }
+        var size = addrNodes.length;
+        if(size ==0) {
+            console.log("addr node is not exist");
+            res.end();
+            return;
+        }
+        //3. pick one service provider
+        var addrPath = servicePath + '/';
+        if(size == 1) {
+            addrPath += addrNodes[0];
+        }else {
+            addrPath += addrNodes[parseInt(Math.random() * size)];
+        }
+        console.log('addrPath: %s', addrPath);
+        //4. get service addr;
+        zk.getData(addrPath,function(err,serviceAddr) {
+            if(err) {
+                console.log("get service addr data error: " + err.stack);
+                res.end();
+                return;
+            }
+            console.log('serviceAddr: %s', serviceAddr);
+            if(!serviceAddr) {
+                console.log('service addr is not exist');
+                res.end();
+                return;
+            }
+            //5. do inverse http proxy
+            proxy.web(req, res, {
+                target: 'http://' + serviceAddr
+            });
+        });
 
+    });
 
 });
 app.listen(PORT,function(){
